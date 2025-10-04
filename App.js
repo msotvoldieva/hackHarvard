e7f9c052315474a2efe8ba0ca060773378d7a297
Ana Lopes <126198380+aj-lopes@users.noreply.github.com>
2025-10-04 14:00:37 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import EcoPredictDashboard from './dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <EcoPredictDashboard />
+    </div>
+  );
+}
+
+export default App;
